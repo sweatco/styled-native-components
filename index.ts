9ce49032ddae8a50e923dfa5ca986125fb6d1516
Ivan Kuchaev <i.kuchaev@sweatco.in>
2023-12-27 13:54:24 +0000

    Move styled to index file

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-export { createStyled } from './src/styled'
+import { createStyled } from './src/styled'
+
+export { createStyled }
+
+/**
+ * Override DefaultTheme to get accurate typings for your project.
+ *
+ * ```
+ * // create styled-native-components.d.ts in your project source
+ * // if it isn't being picked up, check tsconfig compilerOptions.types
+ * import 'styled-native-components'
+ * import Theme from './theme'
+ *
+ * type ThemeType = typeof Theme;
+ *
+ * declare module "styled-native-components" {
+ *  export interface DefaultTheme extends ThemeType {}
+ * }
+ * ```
+ */
+export interface DefaultTheme {
+    [key: string]: any
+}
+
+export const { styled, css, ThemeProvider, ThemeContext } = createStyled<DefaultTheme>()
+
+export default styled
--- src/styled.tsx
+++ src/styled.tsx
@@ -177,27 +177,3 @@ export function createStyled<Theme extends AnyTheme>() {
 
   return { styled, ThemeProvider, ThemeContext, css }
 }
-
-/**
- * Override DefaultTheme to get accurate typings for your project.
- *
- * ```
- * // create styled-native-components.d.ts in your project source
- * // if it isn't being picked up, check tsconfig compilerOptions.types
- * import 'styled-native-components'
- * import Theme from './theme'
- *
- * type ThemeType = typeof Theme;
- *
- * declare module "styled-native-components" {
- *  export interface DefaultTheme extends ThemeType {}
- * }
- * ```
- */
-export interface DefaultTheme {
-  [key: string]: any
-}
-
-export const { styled, css, ThemeProvider, ThemeContext } = createStyled<DefaultTheme>()
-
-export default styled
