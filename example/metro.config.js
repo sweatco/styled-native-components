f0243ab794d898d5b268e1b71c318a18b123b5ad
Ivan Kuchaev <i.kuchaev@sweatco.in>
2025-08-12 12:20:15 +0100

    chore: update dependencies and configurations for example project (#10)
    - Updated various dependencies in package.json and example's package.json to their latest versions.
    - Adjusted TypeScript configuration to extend from @tsconfig/react-native.
    - Modified metro.config.js to use react-native-monorepo-config.
    - Updated Android build configurations including targetSdkVersion and kotlinVersion.
    - Enhanced iOS project settings and updated Podfile.lock for new dependencies.
    - Added support for new Ruby libraries in Gemfile.
    - Updated .gitignore to include .kotlin directory.

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,7 +1,5 @@
 const path = require('path')
 const { getDefaultConfig } = require('@react-native/metro-config')
-const { getConfig } = require('react-native-builder-bob/metro-config')
-const pkg = require('../package.json')
 
 const root = path.resolve(__dirname, '..')
 
@@ -11,8 +9,12 @@ const root = path.resolve(__dirname, '..')
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-module.exports = getConfig(getDefaultConfig(__dirname), {
-  root,
-  pkg,
-  project: __dirname,
-})
+module.exports = (async () => {
+  const { withMetroConfig } = await import('react-native-monorepo-config')
+  const baseConfig = getDefaultConfig(__dirname)
+  
+  return withMetroConfig(baseConfig, {
+    root,
+    dirname: __dirname,
+  })
+})()
